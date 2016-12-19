ee3d4fa8dc2fa06a4c98e05ea473286dc6f0918a
zhushuai <zhushuai@allydata.cn>
2016-12-19 19:08:32 +0800

    增加服务器端文件size对比检查防止覆盖其它客户端的提交

--- monitor.php
+++ monitor.php
@@ -9,10 +9,10 @@ function alert($msg) {
     file_put_contents('/tmp/notify_box.sh',sprintf("DISPLAY=:0.0 notify-send '%s' '%s'".PHP_EOL,'KeePass Upload', $msg), FILE_APPEND);
 }
 
-$files = scandir($fileRoot);
-foreach ($files as $k => $v) {
+$localFiles = scandir($fileRoot);
+foreach ($localFiles as $k => $v) {
     if (in_array($v, ['.','..'])) {
-        unset($files[$k]);
+        unset($localFiles[$k]);
     }
 }
 
@@ -22,30 +22,48 @@ if ($cache === false) {
     $needUpload = true;
 }
 $cacheArr = json_decode($cache, TRUE);
+$allCacheArr = $cacheArr;
 if (empty($cacheArr)) {
     $needUpload = true;
 }
-foreach ($files as $k => $file) {
-    if (isset($cacheArr[$file]) && md5_file($fileRoot.'/'.$file) == $cacheArr[$file]) {
-        unset($files[$k]);
+foreach ($localFiles as $k => $file) {
+    if (isset($cacheArr[$file]) && filesize($fileRoot.'/'.$file) == $cacheArr[$file]) {
+        unset($localFiles[$k]);
     }
 }
-if (count($files) > 0) {
+//本地文件对比cache中还有剩余，说明有新增或更新
+if (count($localFiles) > 0) {
     $needUpload = true;
 }
-
-//update cache
 if ($needUpload) {
-    $tmp = [];
-    foreach ($files as $v) {
-        $tmp[$v] = md5_file($fileRoot.'/'.$v);
-    }
-    $writeResult = file_put_contents($currentPath.'/cache.php', json_encode($tmp));
-    if ($writeResult === false) {
-        alert('error while writing cache file');
+
+    //check server
+    $serverInfoShell = sprintf('proxychains bash %s/dropbox_uploader.sh list', $currentPath);
+    exec($serverInfoShell, $outputs);
+    $serverFileInfo = [];
+    foreach ($outputs as $row) {
+        //不支持文件夹验证
+        if (stripos($row, '[f]') !== false) {
+            $row = trim(preg_replace('/ +/', ' ', $row));
+            $tmp = explode(' ',trim($row));
+            $serverFileInfo[$tmp[2]] = $tmp[1];
+        }
     }
 }
 
+//检查本地cache与服务器端是否对应
+foreach ($localFiles as $file) {
+    if (isset($serverFileInfo[$file]) && isset($allCacheArr[$file]) && $allCacheArr[$file] !== $serverFileInfo[$file]) {
+        echo date('Y-m-d H:i:s');
+        $errorInfo = $file.'更新有问题，服务器端文件不是本地上次的文件,请手动更新后再上传';
+        echo $errorInfo.PHP_EOL;
+        alert($errorInfo);
+        exit;
+    }
+}
+
+
+//really upload
 if ($needUpload) {
     $shell = sprintf('proxychains bash %s/dropbox_uploader.sh upload %s/* /', $currentPath, $config['fileroot']);
     echo date('Y-m-d H:i:s');
@@ -56,5 +74,18 @@ if ($needUpload) {
     }
 } else {
     echo date('Y-m-d H:i:s').'No need to upload'.PHP_EOL;
+}
+
+
+//update cache
+if ($needUpload) {
+    $tmp = [];
+    foreach ($localFiles as $v) {
+        $tmp[$v] = filesize($fileRoot.'/'.$v);
+    }
+    $writeResult = file_put_contents($currentPath.'/cache.php', json_encode($tmp));
+    if ($writeResult === false) {
+        alert('error while writing cache file');
+    }
 }
 
